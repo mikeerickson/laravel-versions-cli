4b435e0f0269ec8f2b0dc4c9ba2370acf24a57c1
Mike Erickson <codedungeon@gmail.com>
2021-02-14 09:30:54 -0800

    updated gunner dependency

--- index.js
+++ index.js
@@ -3,18 +3,20 @@
 const { CLI, colors } = require('@codedungeon/gunner')
 const pkgInfo = require('./package.json')
 
+const examples = [
+  `${pkgInfo.packageName} (or lv) ${colors.magenta('(executes using default parameters)')}`,
+  `${pkgInfo.packageName} (or lv) info --versions 7,8 ${colors.magenta('(shows version 7,8)')}`,
+  `${pkgInfo.packageName} (or lv) info --limit 2 ${colors.magenta('(returns 2 rows)')}`,
+  `${pkgInfo.packageName} (or lv) info --show-future false ${colors.magenta('(suppress future releases)')}`,
+]
+
+const exampleInfo = examples.join('\n  ')
+
 const app = new CLI(process.argv, __dirname)
   .usage(
-    `
-  ${pkgInfo.packageName} info --limit 5
+    `${pkgInfo.packageName} info --limit 5
   lv info --limit 5 ${colors.magenta('(uses `lv` alias)')}`,
   )
   .options(/* if not called, options will be suppressed in help dialog */)
-  .examples(
-    `
-  ${pkgInfo.packageName} (or lv) ${colors.magenta('(executes using default parameters)')}
-  ${pkgInfo.packageName} (or lv) info --versions 7,8 ${colors.magenta('(shows version 7,8)')}
-  ${pkgInfo.packageName} (or lv) info --limit 2 ${colors.magenta('(returns 2 rows)')},
-  ${pkgInfo.packageName} (or lv) info --show-future false ${colors.magenta('(suppress future releases)')}`,
-  )
+  .examples(exampleInfo)
   .run({ default: 'info' })
