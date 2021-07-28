1e37ebe2089baf88633e0d14058c56b5c518298d
Mike Erickson <codedungeon@gmail.com>
2021-07-28 09:06:07 -0700

    updated gunner, implemented logging

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const { CLI, colors } = require('@codedungeon/gunner')
 const pkgInfo = require('./package.json')
+let parseArgs = require('minimist')
 
 const examples = [
   `${pkgInfo.packageName} (or lv) ${colors.magenta('(executes using default parameters)')}`,
@@ -12,6 +13,11 @@ const examples = [
 
 const exampleInfo = examples.join('\n  ')
 
+const getLogDirectory = (argv, defaultLocation = 'system') => {
+  let logDir = parseArgs(argv)['logDir'] || parseArgs(argv)['log-dir'] || ''
+  return logDir.length > 0 ? logDir : defaultLocation
+}
+
 const app = new CLI(process.argv, __dirname, pkgInfo)
   .usage(
     `${pkgInfo.packageName} info --limit 5
@@ -19,4 +25,14 @@ const app = new CLI(process.argv, __dirname, pkgInfo)
   )
   .options(/* if not called, options will be suppressed in help dialog */)
   .examples(exampleInfo)
+  .logger({ directory: getLogDirectory(process.argv), alwaysLog: true })
+  .hooks({
+    beforeExecute: (toolbox, command = '', args = {}) => {
+      toolbox.print.write('debug', { hook: 'beforeExecute', command, args, cwd: process.cwd() })
+    },
+    afterExecute: (toolbox, command = '', args = {}) => {
+      toolbox.print.write('debug', { hook: 'afterExecute', command, args })
+    },
+    commandPrefix: 'make:',
+  })
   .run({ default: 'info' })
